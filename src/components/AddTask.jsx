0247a6f73ac82583dff7915848df2754dfc03831
samuelTedeschi <samueltedeschi100@yahoo.com>
2022-08-17 17:35:08 -0300

    1.0.1

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 import './AddTask.css'
 
-const AddTask = () => {
+const AddTask = ({handleTaskAddition}) => {
+    const [inputData, setInputData] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+    } 
+
+    const handleAddTaskClick = () => {
+        handleTaskAddition(inputData)
+        setInputData("")
+    }
+
     return(
         <div className='add-task-container'>
             
-            <input type="text" name="" id="" className='add-task-input'/>
-            <div className="add-task-button-container">
+            <input 
+                    type="text" 
+                    value={inputData}
+                    onChange={handleInputChange}
+                    className='add-task-input'
+            />
 
-            <Button>Adicionar</Button>
+            <div className="add-task-button-container">
+                <Button onClick={handleAddTaskClick}>Adicionar</Button>
             </div>
         </div>
     )
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import './Task.css';
 
-const Task = ({task}) => {
-    return  <div className='task-container'>
-        {task.title}
-    </div> ;
+const Task = ({task, handleTaskClick}) => {
+
+    return (
+        <div
+            className='task-container'
+            style={task.completed ? { borderLeft: "6px solid red"} : {}}
+        >
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+				{task.title}
+			</div>
+        </div>
+    )
+
+   // return  <div className='task-container'> {task.title} </div> ;
 }
  
 export default Task;
