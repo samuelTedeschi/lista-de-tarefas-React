e2540ebf306b4e53b285f1c3aaed8a297df5b55b
samuelTedeschi <samueltedeschi100@yahoo.com>
2022-08-25 09:41:43 -0300

    1.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
@@ -50,18 +51,27 @@ const App = () =>  {
   }
 
   return(
-    <>
+    <Router>
       <div className="container">
         <Header/>
-        <AddTask handleTaskAddition={handleTaskAddition}  />
-        <Tasks
-         tasks={tasks} 
-         handleTaskClick={handleTaskClick}  
-         handleTaskDeletin={handleTaskDeletin} 
-         />
+          <Route path="/" 
+          //exact 
+          render={() => (
+              <>
+                <AddTask handleTaskAddition={handleTaskAddition}  />
+                <Tasks
+                  tasks={tasks} 
+                  handleTaskClick={handleTaskClick}  
+                  handleTaskDeletin={handleTaskDeletin} 
+                />
+              </>
+            )} 
+          />
+
+        
       </div>
     
-    </>
+    </Router>
   ) 
 }
 
